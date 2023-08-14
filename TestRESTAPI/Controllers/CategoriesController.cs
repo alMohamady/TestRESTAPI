0f999e526074734987de14408cb6497383dadb2e
al Mohamady <al.Mohamady47@gmail.com>
2023-08-15 01:40:23 +0300

    09 create post & put methods in web api

--- TestRESTAPI/Controllers/CategoriesController.cs
+++ TestRESTAPI/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TestRESTAPI.Data;
+using TestRESTAPI.Data.Models;
 
 namespace TestRESTAPI.Controllers
 {
@@ -22,6 +23,28 @@ namespace TestRESTAPI.Controllers
         {
             var cats = await _db.Categories.ToListAsync();
             return Ok(cats);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(string category)
+        {
+            Category c = new() { Name = category };
+            await _db.Categories.AddAsync(c);
+            _db.SaveChanges();
+            return Ok(c);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(Category category)
+        {
+            var c = await _db.Categories.SingleOrDefaultAsync(x => x.Id == category.Id);
+            if (c == null)
+            {
+                return NotFound($"Category Id {category.Id} not exists ");
+            }
+            c.Name = category.Name;
+            _db.SaveChanges();
+            return Ok(c);
         }
     }
 }
