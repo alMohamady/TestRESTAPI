57929045e566751be0ed0568a4ef98ccafd2d462
al Mohamady <al.Mohamady47@gmail.com>
2023-08-15 02:22:25 +0300

    010 create delete method in web api

--- TestRESTAPI/Controllers/CategoriesController.cs
+++ TestRESTAPI/Controllers/CategoriesController.cs
@@ -46,5 +46,19 @@ namespace TestRESTAPI.Controllers
             _db.SaveChanges();
             return Ok(c);
         }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> RemoveCategory(int id)
+        {
+            var c = await _db.Categories.SingleOrDefaultAsync(x => x.Id == id);
+            if (c == null)
+            {
+                return NotFound($"Category Id {id} not exists ");
+            }
+            _db.Categories.Remove(c);
+            _db.SaveChanges();
+            return Ok(c);
+        }
+
     }
 }
