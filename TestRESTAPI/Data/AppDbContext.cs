fe0f655e57350dcb0d92db46e7468feb9058b440
al Mohamady <al.Mohamady47@gmail.com>
2023-08-14 17:25:58 +0300

    006 Add database ConnectionString

---
+++ TestRESTAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestRESTAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>  options) : base(options) 
+        { 
+        
+        }
+
+
+    }
+}
