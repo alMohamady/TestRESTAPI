d49c7e9522be535fbe5819a322d554dccb48ec49
al Mohamady <al.Mohamady47@gmail.com>
2023-08-16 15:29:38 +0300

    012 testing APIs methods with Postman

--- TestRESTAPI/Controllers/CategoriesController.cs
+++ TestRESTAPI/Controllers/CategoriesController.cs
@@ -47,7 +47,7 @@ namespace TestRESTAPI.Controllers
             return Ok(c);
         }
 
-        [HttpDelete("id")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveCategory(int id)
         {
             var c = await _db.Categories.SingleOrDefaultAsync(x => x.Id == id);
