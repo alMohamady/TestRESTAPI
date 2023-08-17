71d67f7e1b96633b6b0b1fa1da8f01ed327d5d8a
al Mohamady <al.Mohamady47@gmail.com>
2023-08-17 13:25:12 +0300

    019 using edit with put method

--- TestRESTAPI/Controllers/ItemsController.cs
+++ TestRESTAPI/Controllers/ItemsController.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using TestRESTAPI.Data;
 using TestRESTAPI.Data.Models;
 using TestRESTAPI.Models;
+using static Azure.Core.HttpHeader;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TestRESTAPI.Controllers
 {
@@ -63,6 +65,34 @@ namespace TestRESTAPI.Controllers
             };
             await _db.Items.AddAsync(item);
             await _db.SaveChangesAsync();
+            return Ok(item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, [FromForm] mdlItem mdl)
+        {
+            var item = await _db.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound($"  item id {id} not exists !");
+            }
+            var isCategoryExists = await _db.Categories.AnyAsync(x => x.Id == mdl.CategoryId);
+            if (isCategoryExists)
+            {
+                return NotFound($"  Category id {id} not exists !");
+            }
+            if (mdl.Image != null)
+            {
+                using var stream = new MemoryStream();
+                await mdl.Image.CopyToAsync(stream);
+                item.Image = stream.ToArray();
+            }
+            item.Name = mdl.Name;
+            item.Price = mdl.Price;
+            item.Notes = mdl.Notes;
+            item.CategoryId = mdl.CategoryId;
+
+            _db.SaveChanges();
             return Ok(item);
         }
 
